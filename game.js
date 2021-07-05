a6cd979c6d1526a1db115cf73783a9e4edded22b
trishakshah <79252404+trishakshah@users.noreply.github.com>
2021-07-05 14:55:01 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -10,3 +10,32 @@ function send(){
     document.getElementById("number1").value="";
     document.getElementById("number2").value="";
 }
+
+questionTurn="player1";
+answerTurn="player2";
+score1=0;
+score2=0;
+
+function check(){
+    getAnswer=document.getElementById("answerInput").value;
+    if (getAnswer==actualAnswer){
+        if (answerTurn=="player1"){
+            score1=score1+1;
+            document.getElementById("score1").innerHTML=score1;
+        }
+        else 
+        {score2=score2+1;
+        document.getElementById("score2").innerHTML=score2;
+        }
+    }
+    if (questionTurn=="player1"){
+        questionTurn="player2";
+        document.getElementById("playerQ").innerHTML="Question - "+name2;
+        answerTurn="player1";
+        document.getElementById("playerA").innerHTML="Answer - "+name1;
+    }
+    else questionTurn="player1";
+    document.getElementById("playerQ").innerHTML="Question - "+name1;
+    answerTurn="player2";
+    document.getElementById("playerA").innerHTML="Answer - "+name2;
+}
--- login.js
+++ login.js
@@ -3,4 +3,5 @@ user1=document.getElementById("input1").value;
 user2=document.getElementById("input2").value;
 localStorage.setItem("user1",user1);
 localStorage.setItem("user2",user2);
+window.location="game.html";
 }
